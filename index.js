e3a6e49cea9a29292867bebb7196a23a6226d226
DiegoBastos40 <diegobastosherval@gmail.com>
2022-01-28 21:54:50 +0000

    database-cards

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+      startGame(); 
+    };
+    document.getElementById('accessible-game').onclick = () => {
+        startAcessGame(); 
+      };
+    function startGame() {
+     }
+    
+    function startAcessGame() {
+
+    }
+  };
