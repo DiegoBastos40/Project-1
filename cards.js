4e09f63ade27c4a5514ca519fb2c1e45426d1100
DiegoBastos40 <diegobastosherval@gmail.com>
2022-01-31 16:12:50 +0000

    di

--- cards.js
+++ cards.js
@@ -1,4 +1,4 @@
-/*  export default [
+  export default [
     { name: 'bulbasur', img: '/images/bulbasaur.png' },
     { name: 'butterfree', img: '/images/butterfree.png' },
     { name: 'caterpie', img: '/images/caterpie.png' },
@@ -23,9 +23,9 @@
     { name: 'cubone', img: '/images/cubone.png' },
     { name: 'dewgong', img: '/images/dewgong.png' },
     { name: 'diglett', img: '/images/diglett.png' },
-  ];  */
+  ];  
 
-  const cards = [
+ /*  const cards = [
     { name: 'bulbasur', img: '/images/bulbasaur.png' },
     { name: 'butterfree', img: '/images/butterfree.png' },
     { name: 'caterpie', img: '/images/caterpie.png' },
@@ -55,4 +55,4 @@
   cards.forEach((card)=>{
     console.log(Math.floor(Math.random(card.img)));
 
-  });
+  }); */
