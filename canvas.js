e50019e00c0455ab4402114e38049c16eacc9a3e
DiegoBastos40 <diegobastosherval@gmail.com>
2022-01-31 14:30:01 +0000

    crear the emphty space

--- canvas.js
+++ canvas.js
@@ -11,12 +11,12 @@ canvas.height = window_height;
 
 //canvas.style.background = '#bbf'
 //Thing About Do
-/* let imgSrcs = ['/unamed.png']
+let imgSrcs = ['/cards.js']
 
-for (let i = 0; i< cards.length ; i++){
+/*for (let i = 0; i< cards.length ; i++){
   this.cards.push(new Card(x, y, width, height, imgSrcs[i]))
-} 
- */
+} */
+ 
 
 class Rectangle {
     constructor(xpoint,ypoint,width,height){
@@ -28,9 +28,33 @@ class Rectangle {
 
     }
      draw(context) {
-        this.background.src = '/images/bulbasaur.png';
+        this.background.src = '/unnamed.png';
         this.background.onload = () => {
-        context.drawImage(this.background, 0, 0, 90, 140);
+       context.drawImage(this.background, 0, 0, 90, 140);
+        context.drawImage(this.background, 100, 0, 90, 140);
+        context.drawImage(this.background, 200, 0, 90, 140);
+        context.drawImage(this.background, 300, 0, 90, 140);
+        context.drawImage(this.background, 400, 0, 90, 140);
+        context.drawImage(this.background, 500, 0, 90, 140);
+        context.drawImage(this.background, 0, 150, 90, 140);
+        context.drawImage(this.background, 100, 150, 90, 140);
+        context.drawImage(this.background, 200, 150, 90, 140);
+        context.drawImage(this.background, 300, 150, 90, 140);
+        context.drawImage(this.background, 400, 150, 90, 140);
+        context.drawImage(this.background, 500, 150, 90, 140);
+        context.drawImage(this.background, 0,300, 90, 140);
+        context.drawImage(this.background, 100, 300, 90, 140);
+        context.drawImage(this.background, 200, 300, 90, 140);
+        context.drawImage(this.background, 300, 300, 90, 140);
+        context.drawImage(this.background, 400, 300, 90, 140);
+        context.drawImage(this.background, 500, 300, 90, 140);
+        context.drawImage(this.background, 0, 450, 90, 140);
+        context.drawImage(this.background, 100, 450, 90, 140);
+        context.drawImage(this.background, 200, 450, 90, 140);
+        context.drawImage(this.background, 300, 450, 90, 140);
+        context.drawImage(this.background, 400, 450, 90, 140);
+        context.drawImage(this.background, 500, 450, 90, 140);
+
        
         }; 
        
--- game.js
+++ game.js
@@ -29,7 +29,7 @@ export default class Game {
     };
   }
   render() {
-    //this.drawBackground();
+   // this.drawBackground();
     //this.drawCardsImage();
   }
 
